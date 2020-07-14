f966fac152d387c511bee3dd681924de274ecc55
Lyman <thelemospace@gmail.com>
2020-07-14 17:03:42 +0300

    commit

--- task2.2/js/script.js
+++ task2.2/js/script.js
@@ -1,20 +1,48 @@
 $(function() {
-    $("#dialog").dialog({
-        autoOpen: false,
-        height: "auto",
-        width: 400,
-        modal: true,
-        buttons: {
-            Yes: function() {
-                $(this).dialog("close");
-            },
-            No: function() {
-                $(this).dialog("close");
-            },
-        },
-    });
+    var dateFormat = "mm/dd/yy",
+        from = $("#from")
+        .datepicker({
+            showAnim: "slide",
+            defaultDate: "+1w",
+            changeMonth: true,
+            numberOfMonths: 3,
+            showButtonPanel: true,
+            changeMonth: true,
+            changeYear: true,
+            showOn: "both",
+            buttonImage: "images/calendar.gif",
+            buttonImageOnly: true,
+            buttonText: "Select date"
+        })
 
-    $("#opener").on("click", function() {
-        $("#dialog").dialog("open");
-    });
+    .on("change", function() {
+            to.datepicker("option", "minDate", getDate(this));
+        }),
+        to = $("#to").datepicker({
+            showAnim: "clip",
+            defaultDate: "+1w",
+            changeMonth: true,
+            numberOfMonths: 3,
+            showButtonPanel: true,
+            changeMonth: true,
+            changeYear: true,
+            showOn: "both",
+            buttonImage: "images/calendar.gif",
+            buttonImageOnly: true,
+            buttonText: "Select date"
+        })
+        .on("change", function() {
+            from.datepicker("option", "maxDate", getDate(this));
+        });
+
+    function getDate(element) {
+        var date;
+        try {
+            date = $.datepicker.parseDate(dateFormat, element.value);
+        } catch (error) {
+            date = null;
+        }
+
+        return date;
+    }
 });

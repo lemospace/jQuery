46f1a58cf6370d71aca97236af7082018ee479e3
Lyman <thelemospace@gmail.com>
2020-07-15 21:26:31 +0300

    commit

--- task2.4/js/script.js
+++ task2.4/js/script.js
@@ -1,27 +1,61 @@
 $(function() {
-    $("#datepicker").datepicker();
-});
-$(function() {
-    var availableTags = ["Kharkiv", "Kyiv", "Odessa", "Lviv"];
-    $("#airport").autocomplete({
-        source: availableTags,
-    });
-});
-$(function() {
-    $(".controlgroup").controlgroup({
-        direction: "vertical",
-    });
-});
 
-$(function() {
-    $(".checkboxradio input").checkboxradio({
-        icon: false,
-    });
-});
+    // There's the gallery and the trash
+    var $gallery = $("#gallery"),
+        $trash = $("#trash");
 
-$(function() {
-    $(".button button").button({
-        icon: "ui-icon-arrowthick-1-e",
-        iconPosition: "end",
+    // Let the gallery items be draggable
+    $("li", $gallery).draggable({
+        cancel: "a.ui-icon", // clicking an icon won't initiate dragging
+        revert: "invalid", // when not dropped, the item will revert back to its initial position
+        containment: "document",
+        helper: "clone",
+        cursor: "move"
     });
+
+    // Let the trash be droppable, accepting the gallery items
+    $trash.droppable({
+        accept: "#gallery > li",
+        classes: {
+            "ui-droppable-active": "ui-state-highlight"
+        },
+        drop: function(event, ui) {
+            deleteImage(ui.draggable);
+        }
+    });
+
+    // Let the gallery be droppable as well, accepting items from the trash
+    $gallery.droppable({
+        accept: "#trash li",
+        classes: {
+            "ui-droppable-active": "custom-state-active"
+        },
+        drop: function(event, ui) {
+            recycleImage(ui.draggable);
+        }
+    });
+    // Image deletion function
+    var recycle_icon = "<a href='link/to/recycle/script/when/we/have/js/off' title='Recycle this image' class='ui-icon ui-icon-refresh'>Recycle image</a>";
+
+    function deleteImage($item) {
+        $item.fadeOut(function() {
+            var $list = $("ul", $trash).length ?
+                $("ul", $trash) :
+                $("<ul class='gallery ui-helper-reset'/>").appendTo($trash);
+
+            $item.find("a.ui-icon-trash").remove();
+            $item.append(recycle_icon).appendTo($list).fadeIn(function() {
+                $item
+                    .animate({ width: "48px" })
+                    .find("img")
+                    .animate({ height: "36px" });
+            });
+        });
+    }
+
+
+
+
+
+
 });

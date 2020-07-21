ae0b5cdd01db1776bf1ab4c09f01309d93c96968
Lyman <thelemospace@gmail.com>
2020-07-21 16:08:20 +0300

    commit

--- task2.9/js/script.js
+++ task2.9/js/script.js
@@ -73,3 +73,57 @@ jQuery(document).ready(function($) {
         return false;
     });
 });
+/*
+$(document).ready(function() {
+    $('.next').on('click', function() {
+        var currentImg = $('.active');
+        var nextImg = currentImg.next();
+
+        if (nextImg.length) {
+            currentImg.removeClass('active').css('z-index', -10);
+            nextImg.addClass('active').css('z-index', 10);
+        }
+    });
+
+
+    $('.prev').on('click', function() {
+        var currentImg = $('.active');
+        var prevImg = currentImg.prev();
+
+        if (prevImg.length) {
+            currentImg.removeClass('active').css('z-index', -10);
+            prevImg.addClass('active').css('z-index', 10);
+        }
+    });
+});*/
+$(document).ready(function() {
+    $('.next').click(function() {
+        var currentImg = $('.active');
+        var currentImgIndex = $('.active').index();
+        var nextImgIndex = currentImgIndex + 1;
+        var nextImg = $('.img').eq(nextImgIndex);
+        currentImg.fadeOut(500);
+        currentImg.removeClass('active');
+
+        if (nextImgIndex == ($('.img:last').index() + 1)) {
+            $('.img').eq(0).fadeIn(500);
+            $('.img').eq(0).addClass('active');
+        } else {
+            nextImg.fadeIn(500);
+            nextImg.addClass('active');
+        }
+
+    });
+
+    $('.prev').click(function() {
+        var currentImg = $('.active');
+        var currentImgIndex = $('.active').index();
+        var prevImgIndex = currentImgIndex - 1;
+        var prevImg = $('.img').eq(prevImgIndex);
+        currentImg.fadeOut(500);
+        currentImg.removeClass('active');
+        prevImg.fadeIn(500);
+        prevImg.addClass('active');
+    });
+
+});
